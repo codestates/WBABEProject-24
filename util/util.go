4ec34831b6ff18eac83f922a422ef817473f15b3
aramirr92 <aramirr92@gmail.com>
2022-12-25 11:30:24 +0900

    [add] 시퀀스 문자열 생성 함수 추가

--- util/util.go
+++ util/util.go
@@ -1,5 +1,10 @@
 package util
 
+import (
+	"fmt"
+	"time"
+)
+
 func NewTrue() *bool {
 	b := true
 	return &b
@@ -8,4 +13,8 @@ func NewTrue() *bool {
 func NewFalse() *bool {
 	b := false
 	return &b
+}
+
+func CreateSeqStr(count uint32) string {
+	return fmt.Sprintf("%d-%010d", time.Now().Unix(), count)
 }
